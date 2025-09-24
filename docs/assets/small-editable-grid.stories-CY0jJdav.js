db3eef030917b9cffdfc7ac1d273f9de4132e1f6
pppfeeling <52871126+pppfeeling@users.noreply.github.com>
2025-09-24 01:46:08 +0000

    Deploying to gh-pages from  @ aad935d7312f09456ec3eebbe3523402acbfee55 🚀

--- docs/assets/small-editable-grid.stories-CY0jJdav.js
+++
@@ -1,8 +0,0 @@
-import{R as e}from"./iframe-DNtM7szx.js";import{D as i}from"./data-editor-all-NenJfBez.js";import{B as m,D as d,u as p,d as c}from"./utils-B2SM65BO.js";import{S as u}from"./story-utils-7kNrYx-7.js";import"./preload-helper-C1FmrZbK.js";import"./image-window-loader-V3wfVhga.js";import"./throttle-DwxcRZC0.js";import"./marked.esm-Bk15hlo4.js";import"./flatten-Cfvwb28E.js";import"./scrolling-data-grid-Cva-q6bk.js";import"./index-D_kXk1yT.js";import"./throttle--dN168Gr.js";const _={title:"Glide-Data-Grid/DataEditor Demos",decorators:[r=>e.createElement(u,null,e.createElement(m,{title:"Editable Grid",description:e.createElement(d,null,"Data grid supports overlay editors for changing values. There are bespoke editors for numbers, strings, images, booleans, markdown, and uri.")},e.createElement(r,null)))]},t=()=>{const{cols:r,getCellContent:s,setCellValue:n}=p(6,!1);return e.createElement(i,{...c,getCellContent:s,columns:r,rows:20,onCellEdited:n})};var o,a,l;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`() => {
-  const {
-    cols,
-    getCellContent,
-    setCellValue
-  } = useMockDataGenerator(6, false);
-  return <DataEditor {...defaultProps} getCellContent={getCellContent} columns={cols} rows={20} onCellEdited={setCellValue} />;
-}`,...(l=(a=t.parameters)==null?void 0:a.docs)==null?void 0:l.source}}};const v=["SmallEditableGrid"];export{t as SmallEditableGrid,v as __namedExportsOrder,_ as default};
--- docs/assets/uneven-rows.stories-C4Ywq84E.js
+++
@@ -1,7 +0,0 @@
-import{R as e}from"./iframe-DNtM7szx.js";import{D as m}from"./data-editor-all-NenJfBez.js";import{B as i,D as c,P as p,u,d}from"./utils-B2SM65BO.js";import{S as w}from"./story-utils-7kNrYx-7.js";import"./preload-helper-C1FmrZbK.js";import"./image-window-loader-V3wfVhga.js";import"./throttle-DwxcRZC0.js";import"./marked.esm-Bk15hlo4.js";import"./flatten-Cfvwb28E.js";import"./scrolling-data-grid-Cva-q6bk.js";import"./index-D_kXk1yT.js";import"./throttle--dN168Gr.js";const H={title:"Glide-Data-Grid/DataEditor Demos",decorators:[r=>e.createElement(w,null,e.createElement(i,{title:"Uneven Rows",description:e.createElement(c,null,"Rows can be made uneven by passing a callback to the ",e.createElement(p,null,"rowHeight")," prop")},e.createElement(r,null)))]},t=()=>{const{cols:r,getCellContent:l}=u(6);return e.createElement(m,{...d,rowHeight:o=>o%3===0?30:o%2?50:60,getCellContent:l,columns:r,rows:1e3})};var a,n,s;t.parameters={...t.parameters,docs:{...(a=t.parameters)==null?void 0:a.docs,source:{originalSource:`() => {
-  const {
-    cols,
-    getCellContent
-  } = useMockDataGenerator(6);
-  return <DataEditor {...defaultProps} rowHeight={r => r % 3 === 0 ? 30 : r % 2 ? 50 : 60} getCellContent={getCellContent} columns={cols} rows={1000} />;
-}`,...(s=(n=t.parameters)==null?void 0:n.docs)==null?void 0:s.source}}};const S=["UnevenRows"];export{t as UnevenRows,S as __namedExportsOrder,H as default};
