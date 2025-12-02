65c8910c8f52f50183e7114c497315b6e0b70fe2
pppfeeling <52871126+pppfeeling@users.noreply.github.com>
2025-12-02 14:31:22 +0000

    Deploying to gh-pages from  @ f91789937c10ad7e9d20be3cee66eafb93adfd19 🚀

--- docs/assets/small-editable-grid.stories-Cd-tumMo.js
+++
@@ -1,8 +0,0 @@
-import{R as e}from"./iframe-CZO0vn9F.js";import{D as i}from"./data-editor-all-DDHTqgPo.js";import{B as m,D as d,u as p,d as c}from"./utils-DRCGzv-2.js";import{S as u}from"./story-utils-C5OIDpnz.js";import"./preload-helper-C1FmrZbK.js";import"./image-window-loader-RbnO5SH5.js";import"./throttle-CQSe6QBF.js";import"./marked.esm-BlDfTzuu.js";import"./flatten-DWMtsgcZ.js";import"./scrolling-data-grid-CX_GCxE4.js";import"./index-D_kXk1yT.js";import"./throttle--dN168Gr.js";const _={title:"Glide-Data-Grid/DataEditor Demos",decorators:[r=>e.createElement(u,null,e.createElement(m,{title:"Editable Grid",description:e.createElement(d,null,"Data grid supports overlay editors for changing values. There are bespoke editors for numbers, strings, images, booleans, markdown, and uri.")},e.createElement(r,null)))]},t=()=>{const{cols:r,getCellContent:s,setCellValue:n}=p(6,!1);return e.createElement(i,{...c,getCellContent:s,columns:r,rows:20,onCellEdited:n})};var o,a,l;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`() => {
-  const {
-    cols,
-    getCellContent,
-    setCellValue
-  } = useMockDataGenerator(6, false);
-  return <DataEditor {...defaultProps} getCellContent={getCellContent} columns={cols} rows={20} onCellEdited={setCellValue} />;
-}`,...(l=(a=t.parameters)==null?void 0:a.docs)==null?void 0:l.source}}};const v=["SmallEditableGrid"];export{t as SmallEditableGrid,v as __namedExportsOrder,_ as default};
