aed9c71a57e05bfc550e39b811ea18bc22474469
jspark <pppfeeling@gmail.com>
2026-02-24 16:04:40 +0900

    사용안한 셀 파일 삭제

--- setup-react-18-test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-npm i -D react@latest react-dom@latest @testing-library/react@latest @testing-library/react-hooks@latest @testing-library/user-event@14.5.1 react-test-renderer@latest @testing-library/dom
--- setup-react-19-test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-npm i -D react@19 react-dom@19 @testing-library/react@latest @testing-library/react-hooks@latest @testing-library/user-event@latest react-test-renderer@latest @testing-library/dom
